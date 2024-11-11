567018144a7518150dd4ab7fcb594ec7e8fa0150
yinweikang <Jasper_yin@xnebula.com>
2024-11-11 14:31:11 +0800

    首页布局

--- Todo/ViewModels/IndexViewModel.cs
+++ Todo/ViewModels/IndexViewModel.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using Todo.Common.Models;
 
 namespace Todo.ViewModels
 {
-    public class IndexViewModel
+    public class IndexViewModel : BindableBase
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public IndexViewModel()
+        {
+            Title = "你好，当前是周一下午13:40";
+            CreateTaskBars();
+        }
+
+
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set { title = value; RaisePropertyChanged(); }
+        }
+
+        private ObservableCollection<TaskBar> taskBars;
+
+        public ObservableCollection<TaskBar> TaskBars
+        {
+            get { return taskBars; }
+            set { taskBars = value; RaisePropertyChanged(); }
+        }
+        void CreateTaskBars()
+        {
+
+            TaskBars = new ObservableCollection<TaskBar>();
+            TaskBars.Add(new TaskBar() { Icon = "ClockFast", Title = "汇总", Color = "#FF0CA0FF", Target = "ToDoView",Content="9"});
+            TaskBars.Add(new TaskBar() { Icon = "ClockCheckOutline", Title = "已完成", Color = "#FF1ECA3A", Target = "ToDoView", Content = "9" });
+            TaskBars.Add(new TaskBar() { Icon = "ChartLineVariant", Title = "完成比例", Color = "#FF02C6DC", Target = "", Content = "9" });
+            TaskBars.Add(new TaskBar() { Icon = "PlaylistStar", Title = "备忘录", Color = "#FFFFA000", Target = "MemoView", Content = "9" });
+        }
     }
 }
