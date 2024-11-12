5bbab39372b72a50eb9d3af2bb53944be9d22504
yinweikang <Jasper_yin@xnebula.com>
2024-11-12 16:20:02 +0800

    待办界面

--- Todo/ViewModels/TodoViewModel.cs
+++ Todo/ViewModels/TodoViewModel.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Todo.Common.Models;
 
 namespace Todo.ViewModels
 {
-    internal class TodoViewModel
+    public class TodoViewModel: BindableBase
     {
+
+        public TodoViewModel()
+        {
+           
+            CreateTodos();
+            AddCommand = new DelegateCommand(Add);
+        }
+        private bool isRightDrawerOpen;
+
+        public bool IsRightDrawerOpen
+        {
+            get { return isRightDrawerOpen; }
+            set { isRightDrawerOpen = value; RaisePropertyChanged(); }
+        }
+
+        private void Add()
+        {
+            IsRightDrawerOpen=!IsRightDrawerOpen;
+        }
+
+        private ObservableCollection<TodoDto> todoDtos;
+
+        public ObservableCollection<TodoDto> TodoDtos
+        {
+            get { return todoDtos; }
+            set { todoDtos = value; RaisePropertyChanged(); }
+        }
+
+        void CreateTodos()
+        {
+            TodoDtos = new ObservableCollection<TodoDto>();
+            for (int i = 0; i < 10; i++)
+            {
+                TodoDtos.Add(new TodoDto() { Title = "待办" + i, Content = "正在处理..." });
+            }
+        }
+
+        public  DelegateCommand AddCommand { get; set; }
     }
 }
