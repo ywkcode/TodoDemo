36c2957a34b1c8c2f02eb30537a0f469dd8e57c5
ywkcode <15251611633@163.com>
2024-11-15 21:25:57 +0800

    submit

---
+++ Todo.API/Repository/IToDoRepository.cs
@@ -0,0 +1,31 @@
+﻿using Todo.API.Context;
+
+namespace Todo.API.Repository
+{
+    public interface ITodoRepository
+    {
+        Task<bool> Add(ToDo todo);
+
+        Task<bool> Update(ToDo todo);
+
+        Task<bool> Delete(int id);
+    }
+
+    public class ToDoRepository : ITodoRepository
+    {
+        public Task<bool> Add(ToDo todo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(ToDo todo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
