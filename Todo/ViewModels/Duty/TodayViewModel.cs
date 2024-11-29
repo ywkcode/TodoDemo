eff20b57116a2e699fa160a935f259611336a1d6
yinweikang <Jasper_yin@xnebula.com>
2024-11-29 17:47:56 +0800

    今日值班安排

--- Todo/ViewModels/Duty/TodayViewModel.cs
+++ Todo/ViewModels/Duty/TodayViewModel.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Todo.IService;
 
 namespace Todo.ViewModels.Duty
 {
     public class TodayViewModel : BindableBase
     {
-        public TodayViewModel()
+        private static Timer timer;
+        private readonly IDutyOrderService orderService;
+        private readonly IDutyPlanService planService;
+
+        #region 属性
+        #endregion
+        public TodayViewModel(IDutyOrderService orderServiceArg,IDutyPlanService planServiceArg)
         {
-                
+            var timerCallback = new TimerCallback(DisplayTimeEvent);
+            timer = new Timer(timerCallback, null, 0, 1000);
+            orderService=orderServiceArg;
+            planService=planServiceArg;
+        }
+        //回调逻辑
+        private async  void DisplayTimeEvent(Object? stateInfo)
+        {
+            var nowdate = DateTime.Now;
+            var currentDate = nowdate.ToString("yyyy-MM-dd");
+
+            var plan =(await planService.GetDataLists()).FirstOrDefault(s=>s.PlanDate== currentDate);
         }
     }
 }
--- Todo/Views/Duty/TodayView.xaml.cs
+++ Todo/Views/Duty/TodayView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Todo.IService;
 
 namespace Todo.Views.Duty
 {
@@ -20,17 +22,12 @@ namespace Todo.Views.Duty
     /// </summary>
     public partial class TodayView : UserControl
     {
-        private static Timer timer;
+      
         public TodayView()
         {
             InitializeComponent();
-            var timerCallback = new TimerCallback(DisplayTimeEvent);
-            timer = new Timer(timerCallback, null, 0, 1000);
+         
         }
-        //回调逻辑
-        private static void DisplayTimeEvent(Object? stateInfo)
-        {
-            Console.WriteLine("The TimerCallback event was raised at {0:HH:mm:ss.fff}", DateTime.Now);
-        }
+       
     }
 }
